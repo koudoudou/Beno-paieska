ffd3cfcd066d9288097a9201456f04dd565dfb50
koudoudou <150090844+koudoudou@users.noreply.github.com>
2024-10-11 17:20:07 +0300

    Few tweaks
    Deleted unnecessary sprites and tweaked ending code

--- beno-paieška/gd files/ending_scene.gd
+++ beno-paieška/gd files/ending_scene.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://tscn_files/main_menu.tscn")
+	State.has_won = false 
 
 
 func _on_exit_pressed() -> void:
